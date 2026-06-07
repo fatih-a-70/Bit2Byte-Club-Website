eb5fd4935f595f0ef045c6b2bc059f427e19a4b6
fatih-a-70 <nazat2207070@stud.kuet.ac.bd>
2026-06-08 02:15:13 +0600

    Create bit2byte_db and add database connection config

---
+++ php/config.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$dbname = "bit2byte_db";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
